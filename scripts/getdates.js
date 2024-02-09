47e6c52c07593cfdb4e8fd9a04347b87bd300fae
ensigntech94 <ensigntechsolutions@gmail.com>
2024-02-09 06:39:24 -0700

    js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,7 +2,7 @@
 const copyrightSymbol = '\u00A9';
 const copyright = document.getElementById("copyright").innerText;
 
-document.getElementById('copyright').innerText = `${copyrightSymbol} 2024 ${copyright}`;
+document.getElementById('copyright').innerText = `Igwe Emmanuel ${copyrightSymbol} 2024 ${copyright}`;
 
 // last modified
 const lastModified = document.lastModified;
