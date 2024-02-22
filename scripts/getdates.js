580563904c09ce89edd74581f106fbbe3de50089
ensigntech94 <ensigntechsolutions@gmail.com>
2024-02-22 11:06:46 -0700

    submit

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,20 @@
 // copyright
 const copyrightSymbol = '\u00A9';
 const copyright = document.getElementById("copyright").innerText;
+const visited = document.querySelector('.visited');
+let numVisits = Number(localStorage.getItem('visits') || 0);
+
+if (numVisits !== 0) {
+    visited.textContent = `Visited: ${numVisits}`;
+}
+else {
+    visited.textContent = 'This is your first time visiting! Welcome!'
+}
+
+numVisits++
+
+localStorage.setItem('visits', numVisits)
+
 
 document.getElementById('copyright').innerText = `Igwe Emmanuel ${copyrightSymbol} 2024 ${copyright}`;
 
