f46f4f6c1a90ec5695fd625290e0816f7f8bc8c6
ensigntech94 <ensigntechsolutions@gmail.com>
2024-02-24 06:06:38 -0700

    discover page update

--- chamber/scripts/toggle.js
+++ chamber/scripts/toggle.js
@@ -1,9 +1,11 @@
 const modeToggle = document.querySelector('#mytoggle');
 const main = document.querySelector('main');
 const nav = document.querySelector('nav');
+const aside = document.querySelector('aside')
 
-modeToggle.addEventListener('change', () => {
-    main.classList.toggle('darkmode');
-    nav.classList.toggle('navdark')
-
+modeToggle.addEventListener('change', ()=> {
+    main.classList.toggle('darkmode')
+    nav.classList.toggle('navigation-dark')
+    aside.classList.toggle('aside-dark')
 })
+ 
