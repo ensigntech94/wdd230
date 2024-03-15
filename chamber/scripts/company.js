392742799812d86c7d10daff28df4c2bdb4414b9
ensigntech94 <ensigntechsolutions@gmail.com>
2024-03-15 05:29:40 -0600

    submit

--- chamber/scripts/company.js
+++ chamber/scripts/company.js
@@ -1,74 +1,70 @@
-const linksURL = "https://heavenlyaura.github.io/wdd230/chamber/data/members.json";
-const layout = document.querySelector('.layout');
-const grid = document.querySelector('.grid-button')
-const list = document.querySelector('.list-button')
+const linksURL = "https://ensigntech94.github.io/wdd230/chamber/data/members.json";
+const layout = document.querySelector(".layout");
+const grid = document.querySelector(".grid-button");
+const list = document.querySelector(".list-button");
 
-document.addEventListener('DOMContentLoaded', () => {
-    layout.classList.add('grid')
-})
+document.addEventListener("DOMContentLoaded", () => {
+  layout.classList.add("grid");
+});
 
 /* button switch */
 
-list.addEventListener('click', () => {
-    layout.classList.add('list');
-    layout.classList.remove('grid');
-})
-grid.addEventListener('click', () => {
-    layout.classList.add('grid')
-    layout.classList.remove('list')
-})
-
+list.addEventListener("click", () => {
+  layout.classList.add("list");
+  layout.classList.remove("grid");
+});
+grid.addEventListener("click", () => {
+  layout.classList.add("grid");
+  layout.classList.remove("list");
+});
 
 async function getCompany(url) {
-    const response = await fetch(url);
+  const response = await fetch(url);
 
-    if (response.ok) {
-        const data = await response.json();
-        // console.table(data);
+  if (response.ok) {
+    const data = await response.json();
+    // console.table(data);
 
-        displayCompany(data.company);
-    }
+    displayCompany(data.company);
+  }
 }
 
 function displayCompany(companies) {
-
-   companies.forEach(company => {
+  companies.forEach((company) => {
     // console.log(company)
-    let name = document.createElement('p')
-    let image = document.createElement('img');
-    let card = document.createElement('div');
-    let address = document.createElement('p');
-    let contact = document.createElement('p');
-    let website = document.createElement('a')
+    let name = document.createElement("p");
+    let image = document.createElement("img");
+    let card = document.createElement("div");
+    let address = document.createElement("p");
+    let contact = document.createElement("p");
+    let website = document.createElement("a");
 
     name.textContent = company.name;
-    name.classList.add('name')
+    name.classList.add("name");
 
     // console.log(company.logo)
     /* set website attribute */
-    website.setAttribute('href', company.website)
-    website.innerHTML = company.website
+    website.setAttribute("href", company.website);
+    website.innerHTML = company.website;
 
-    /* set image attribute */ 
-    image.setAttribute('src', company.logo)
-    image.setAttribute('alt', `${company.name} logo`)
-    image.setAttribute('width', '200')
+    /* set image attribute */
+    image.setAttribute("src", company.logo);
+    image.setAttribute("alt", `${company.name} logo`);
+    image.setAttribute("width", "200");
 
     /* set address and contact */
     address.textContent = company.address;
-    address.classList.add('address')
+    address.classList.add("address");
     contact.textContent = company.contact;
 
+    card.appendChild(name);
+    card.appendChild(image);
+    card.appendChild(address);
+    card.appendChild(contact);
+    card.appendChild(website);
 
-    card.appendChild(name)
-    card.appendChild(image)
-    card.appendChild(address)
-    card.appendChild(contact)
-    card.appendChild(website)
-
-    layout.appendChild(card)
-
-   });
+    layout.appendChild(card);
+  });
 }
 
-getCompany(linksURL)
+getCompany(linksURL);
